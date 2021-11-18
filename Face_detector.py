e3be059c74f2122555811fe05e492a8687cee909
Adwait Patil <adwaitpatil1199@gmail.com>
2021-11-19 00:52:29 +0530

    Added smile detector

--- Face_detector.py
+++ Face_detector.py
@@ -5,6 +5,7 @@ import cv2
 # Loading cascades
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
 
 # Creating a function
 
@@ -20,6 +21,9 @@ def detect(gray,frame):
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1 , 3)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_frame,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
+        smiles = smile_cascade.detectMultiScale(roi_gray,1.7,29)
+        for (sx,sy,sw,sh) in smiles:
+            cv2.rectangle(frame,(sx,sy),(sx+sw,sy+sh),(0,0,255),2)
     return frame
         
 # Face detection using webcam (0) external cam (1)
